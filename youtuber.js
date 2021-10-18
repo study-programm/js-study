437c14a85a94dc944bc8b742c7c2b0dec4388272
study-programm <84725572+study-programm@users.noreply.github.com>
2021-10-18 23:50:54 +0900

    Add files via upload

---
+++ youtuber.js
@@ -0,0 +1,21 @@
+//オブジェクトの複数作る練習
+
+/*しまぶー　あっちゃん　ヒカキン　はじめしゃちょー 
+ビジネス系　エンタメ系
+Youtuber */
+let youtuber = {
+
+    business: [
+        "しまぶー": {
+            "age": 20,
+            "sex": "man",
+            "profession": "Engineer"
+        },
+        "あっちゃん": {
+            profession: "actor"
+        }
+    ],
+
+    
+}
+console.log(youtuber["business"]["しまぶー"])
