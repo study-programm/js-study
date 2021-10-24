6fb535f50966875c6d025cb9812d2c057c3b92e5
study-programm <84725572+study-programm@users.noreply.github.com>
2021-10-24 11:03:21 +0900

    Add files via upload

--- function.js
+++ function.js
@@ -21,164 +21,52 @@ function throwAway(food) {
     delete food;
 }
 
-// returnが必要なパターン
-function isTweetable(text) {
-    return text.length <= 140;
-}
-console.log(isTweetable("eee"));
+//returnが必要なパターン
+// function isTweetable(text) {
+//     return text.length <= 140;
+// }
+// console.log(isTweetable("eee"));
 
-//returnがいらない場合
-function alertTweetable(text) {
-    //テキストの中身が140文字以下の時、you can tweetとアラートを出す
-    // if (text.length <= 140) {
-    //     alert("you can tweet!");
-    // }
-}
-alertTweetable("foo");
+// //returnがいらない場合
+// function alertTweetable(text) {
+//     //キストの中身が140文字以下の時、you can tweetとアラートを出す
+//     if (text.length <= 140) {
+//         alert("you can tweet!");
+//     }
+// }
+// alertTweetable("foo");
 
 //上で宣言しているものがそのまま再利用できる例文を作る
 
-function isAble(openchat) {
-    //140文字以下の時、true 違う場合falseが出る
-    return openchat.length < 140;
+function isTweetable(text) {
+    return text.length < 4;
 }
-console.log(isAble("foo"));
+// console.log(isTweetable("abo"));
 
+function alertTweetable(text2) {
 
-function isAble2(text2) {
-    if (isAble(openchat)) {
-        alert("you can tweet!");
+    //上の条件を関数で持ってくる
+    if (isTweetable(text2)) {
+        window.alert("you can tweet");
     }
 }
 
-console.log(isAble2("foo"));
+// alertTweetable("ts");
 
-function isAble2(text2) {
-    // fooをconsole.logで吐き出すだけ
-}
+//匿名関数 関数名がなくても動く
 
-isAble2("foo");
+const isTweetablew = function (text) {
+    return text.length < 4;
+};
 
+// console.log(isTweetablew("sss"));
 
-// const hensu = 'Global';
-// console.log(hensu);
+//コールバック関数
 
-// function local() {
-//     const hensu = 'Local';
-//     console.log(hensu);
-// }
-// local();
-// console.log(hensu);
+/*引数にコールバック関数を入れている関数を高階関数 */
 
-
-// const owner = "baka";
-// function isAble(text) {
-//     alert(text + " is " + owner);
+//  function 高階関数(コールバック関数){
+//     //処理
+//     //コールバック関数
 // }
 
-// function isAble2(text) {
-//     alert(owner + " is " + text);
-// }
-
-// isAble("owner");
-// isAble2("owner");
-
-//関数の外なのでグローバル
-const owner = "baka";
-
-function isAble(text) {
-    //スコープの中なのでローカル変数
-    const owner = "a";
-    console.log(owner);
-    const Taira = "owner"
-}
-//スコープの中でグローバル変数を読んでもローカル変数の方が優先度上
-isAble();
-
-//グローバル変数はどこでも実行可能
-console.log(owner);
-//ローカル変数はスコープ外で実行しても出ない
-// console.log(Taira);
-
-
-
-// function isAble2(text) {
-//     //あほが代入される
-//     owner = "aho"
-//     //あほはオーナー
-//     alert(owner + " is " + text);
-
-// }
-// isAble("owner");
-// isAble2("owner");
-// isAble("owner");
-
-
-//そういうときこそコードを書くのです
-function sleep() {
-    if (true) { //クソ
-        var goodNight = "おやすみ";
-    }
-    return goodNight;
-}
-console.log(sleep()); // 寝れない
-
-//再宣言
-// var a = 0;
-// var a = 1; 
-// console.log(a) // `1`と出力される
-
-// // let
-// let b = 0;
-// let b = 1;
-
-// // const
-// const c = 0;
-// const c = 1; 
-
-//再代入
-/*
- * var
-*/
-
-var a = 10;
-a = 1;
-console.log(a);
-
-let aa = 1;
-aa = 0;
-console.log(aa);
-
-//エラーになる
-const aaa = 0;
-// aaa = 1;
-// console.log(aaa);
-
-//var
-{
-    var a = 0;
-}
-
-console.log(a);
-//expected 0
-
-//let
-
-{
-    //ブロックスコープにより再宣言にならない
-    let aa = 1;
-    console.log(aa);
-    //expected 1
-}
-
-console.log(aa);
-//expected 0
-
-{
-    //ブロックスコープにより再宣言にならない
-    const aaa = 1;
-    console.log(aaa);
-    //expected 1
-}
-console.log(aaa);
-//expected 0
